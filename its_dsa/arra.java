c127e13860968aa1115a4ffa7a7d3c170a245e3c
Ujjaval-Karangiya <ujjavalk2401.gmail.com>
2025-06-30 15:53:50 +0530

    first update

--- its_dsa/arra.java
+++ its_dsa/arra.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
 public class arra {
-    //print the array 
+    // print the array
     static void print(int array[]) {
+        System.out.print("[");
         for (int i = 0; i < array.length; i++) {
-            System.out.println((i + 1) + ":" + array[i]);
+            System.out.print(" " + array[i] + " ");
         }
+        System.out.print("]");
     }
 
     // for linear search
@@ -17,44 +19,191 @@ public class arra {
 
         }
     }
-    //for find the min max number
-    static void min_max(int array[]){
+
+    // for find the min max number
+    static void min_max(int array[]) {
         int min = Integer.MIN_VALUE;
-         for (int i = 0; i < array.length; i++) {
-            if (min<=array[i]) {
-                min=array[i];
-            }      
-        }
-        System.out.println("minimum is :"+min);
-        int max = Integer.MAX_VALUE;
         for (int i = 0; i < array.length; i++) {
-            if (max>=array[i]) {
-                max=array[i];
+            if (min <= array[i]) {
+                min = array[i];
             }
         }
-        System.out.println("maximum is :"+max);
+        System.out.println("minimum is :" + min);
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (max >= array[i]) {
+                max = array[i];
+            }
+        }
+        System.out.println("maximum is :" + max);
     }
-    //binary search
-    static void binary(int array[],int key){
-        int st = 0 ;
-        int ed = array.length-1;
+
+    // binary search
+    static void binary(int array[], int key) {
+        int st = 0;
+        int ed = array.length - 1;
         while (true) {
-            int mid =(st+ed)/2;
-        if (array[mid]==key) {
-           System.out.println("your target is in "+(mid+1)+" position"); 
-           break;       
-        }else if (array[mid] >= key) {
-            st=mid+1;
-        }else if (array[mid] <= key) {
-            ed=mid-1;
-        }else{
-           System.out.println("invalid");
-           break;
-        }
-            
+            int mid = (st + ed) / 2;
+            if (array[mid] == key) {
+                System.out.println("your target is in " + (mid + 1) + " position");
+                break;
+            } else if (array[mid] >= key) {
+                st = mid + 1;
+            } else if (array[mid] <= key) {
+                ed = mid - 1;
+            } else {
+                System.out.println("invalid");
+                break;
+            }
+
         }
     }
-    //main function that execute code 
+
+    // this is for revese array
+    static void reverse(int array[]) {
+        int fi = 0;
+        int la = array.length - 1;
+        while (fi < la) {
+            int temp = array[fi];
+            array[fi] = array[la];
+            array[la] = temp;
+            fi++;
+            la--;
+        }
+        System.out.println("your array is :");
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" " + array[i] + " ");
+        }
+        System.out.print("]");
+    }
+
+    // pair the array
+    static void pair(int array[]) {
+        System.out.println("it is all posible pairs");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] != array[j]) {
+                    System.out.print("(" + array[i] + "," + array[j] + ")" + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("it is all greter posible pair");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] != array[j]) {
+                    System.out.print("(" + array[i] + "," + array[j] + ")" + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // total sub arrays
+    static void subArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                System.out.print("[");
+                for (int k = i; k < j; k++) {
+                    System.out.print(" " + array[k] + " ");
+                }
+                System.out.print("]");
+            }
+            System.out.println();
+        }
+    }
+
+    // bubble sort
+    static void bubble(int array[]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" " + array[i] + " ");
+        }
+        System.out.print("]");
+
+    }
+
+    // selectionsort
+    static void selection(int array[]) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minpos] < array[j]) {
+                    minpos = j;
+                }
+            }
+            int temp = array[minpos];
+            array[minpos] = array[i];
+            array[i] = temp;
+        }
+
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" " + array[i] + " ");
+        }
+        System.out.print("]");
+    }
+
+    // Insertion Sort
+    static void insertionSort(int array[]) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    // Counting Sort (non-negative only)
+    static void countingSort(int array[]) {
+        int max = Arrays.stream(array).max().orElse(0);
+        int[] count = new int[max + 1];
+        for (int num : array)
+            count[num]++;
+        int index = 0;
+        for (int i = 0; i < count.length; i++)
+            while (count[i]-- > 0)
+                array[index++] = i;
+    }
+
+    // Delete at index
+    static int[] deleteElement(int[] array, int index) {
+        if (index < 0 || index >= array.length)
+            return array;
+        int[] newArr = new int[array.length - 1];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (i != index)
+                newArr[j++] = array[i];
+        }
+        return newArr;
+    }
+
+    // insert at index
+    static int[] insertElement(int[] array, int index, int value) {
+        int[] newArr = new int[array.length + 1];
+        for (int i = 0, j = 0; i < newArr.length; i++) {
+            if (i == index)
+                newArr[i] = value;
+            else
+                newArr[i] = array[j++];
+        }
+        return newArr;
+    }
+
+    // main function that execute code
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int array[] = new int[5];
@@ -67,32 +216,99 @@ public class arra {
                 break;
             }
         }
-        
+
         int num = 1;
-        for(num = 1; num >= 1;num++){
-            System.out.println("\nfor print type 1 \nfor min-max number type 2 \nfor linear search type 3 \nfor binary search type 4 \n for exit type 0 ");
+        for (num = 1; num >= 1; num++) {
+             System.out.println("\n--- Array Operations Menu ---");
+             System.out.println("1. Print Array");
+             System.out.println("2. Min & Max");
+            System.out.println("3. Linear Search");
+            System.out.println("4. Binary Search");
+            System.out.println("5. reverse array");
+            System.out.println("6. pairs");
+            System.out.println("7. subarray");
+            System.out.println("8. Bubble Sort");
+            System.out.println("9. Selection Sort");
+            System.out.println("10. Insertion Sort");
+            System.out.println("11. Counting Sort");
+            System.out.println("12. Insert Element");
+            System.out.println("13. Delete Element");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
             int x = sc.nextInt();
-        switch (x) {
-            case 1 :System.out.println("your array is :");
+            switch (x) {
+                case 1:
+                    System.out.println("your array is :");
                     print(array);
-            break;
-               
-            case 2 :min_max(array);
-            break;    
-                
-            case 3 :System.out.println("your guess num");
+                    break;
+
+                case 2:
+                    min_max(array);
+                    break;
+
+                case 3:
+                    System.out.println("your guess num");
                     int key = sc.nextInt();
                     linear_search(array, key);
-            break;
-            case 4 :System.out.println("your guess num");
+                    break;
+                case 4:
+                    System.out.println("your guess num");
                     int key2 = sc.nextInt();
                     binary(array, key2);
-            break;
+                    break;
+                case 5:
+                    reverse(array);
+                    break;
 
-            default:num=-1;
-                break;
+                case 6:
+                    pair(array);
+                    break;
+
+                case 7:
+                    subArray(array);
+                    break;
+
+                case 8:
+                    bubble(array);
+                    break;
+
+                case 9:
+                    selection(array);
+                    break;
+
+                case 10:    
+                    insertionSort(array);
+                    System.out.println("After Insertion Sort:");
+                    print(array);
+                    break;
+
+                case 11:
+                    countingSort(array);
+                    System.out.println("After Counting Sort:");
+                    print(array);
+                    break; 
+
+                 case 12:
+                    System.out.print("Enter index to insert: ");
+                    int idx = sc.nextInt();
+                    System.out.print("Enter value: ");
+                    int val = sc.nextInt();
+                    array = insertElement(array, idx, val);
+                    print(array);
+                    break;
+
+                case 13:
+                    System.out.print("Enter index to delete: ");
+                    int delIdx = sc.nextInt();
+                    array = deleteElement(array, delIdx);
+                    print(array);
+                    break;    
+                    
+                default:
+                    num = -1;
+                    break;
+            }
         }
-    }
 
     }
 
--- python/find_location.py
+++ python/find_location.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 import phonenumbers
-from phonenumbers import geocoder, carrier, number_type, PhoneNumberType, PhoneNumberFormat
+from phonenumbers import geocoder, carrier, number_type, PhoneNumberType, PhoneNumberFormat, timezone, region_code_for_number
 
 # Get number type as string
 def get_number_type_str(num_type):
@@ -18,17 +18,19 @@ def get_number_type_str(num_type):
 def check_number():
     number = entry.get().strip()
     try:
-        parsed = phonenumbers.parse(number)
+        parsed = phonenumbers.parse(number, "IN")  # Default to India if no country code
         if not phonenumbers.is_valid_number(parsed):
             messagebox.showerror("Invalid", "Not a valid phone number.")
             return
 
         result = {
             "Number": number,
-            "International": phonenumbers.format_number(parsed, PhoneNumberFormat.INTERNATIONAL),
+            "International Format": phonenumbers.format_number(parsed, PhoneNumberFormat.INTERNATIONAL),
             "Region": geocoder.description_for_number(parsed, "en") or "Unknown",
+            "Country Code": region_code_for_number(parsed),
             "Carrier": carrier.name_for_number(parsed, "en") or "Unknown",
-            "Type": get_number_type_str(number_type(parsed))
+            "Line Type": get_number_type_str(number_type(parsed)),
+            "Time Zone": ", ".join(timezone.time_zones_for_number(parsed))
         }
 
         result_text.delete("1.0", tk.END)
@@ -38,19 +40,55 @@ def check_number():
     except phonenumbers.NumberParseException as e:
         messagebox.showerror("Error", str(e))
 
+# Clear input and result
+def clear_fields():
+    entry.delete(0, tk.END)
+    result_text.delete("1.0", tk.END)
+
+# Copy result to clipboard
+def copy_result():
+    text = result_text.get("1.0", tk.END)
+    if text.strip():
+        root.clipboard_clear()
+        root.clipboard_append(text)
+        messagebox.showinfo("Copied", "Result copied to clipboard.")
+    else:
+        messagebox.showwarning("Empty", "No result to copy.")
+
+# Save result to text file
+def save_to_file():
+    text = result_text.get("1.0", tk.END)
+    if text.strip():
+        with open("phone_info.txt", "w") as f:
+            f.write(text)
+        messagebox.showinfo("Saved", "Result saved to phone_info.txt.")
+    else:
+        messagebox.showwarning("Empty", "No result to save.")
+
 # GUI setup
 root = tk.Tk()
-root.title("Phone Number Checker")
-root.geometry("500x300")
+root.title("📞 Phone Number Checker")
+root.geometry("550x400")
 root.resizable(False, False)
 
-ttk.Label(root, text="Enter Phone Number:").pack(pady=10)
+# UI Layout
+ttk.Label(root, text="Enter Phone Number (with or without country code):").pack(pady=10)
+
 entry = ttk.Entry(root, width=40)
 entry.pack()
 
-ttk.Button(root, text="Check", command=check_number).pack(pady=10)
+ttk.Button(root, text="🔍 Check", command=check_number).pack(pady=5)
 
-result_text = tk.Text(root, height=10, width=60, font=("Courier", 10))
+# Result box
+result_text = tk.Text(root, height=10, width=65, font=("Courier", 10))
 result_text.pack(pady=5)
+
+# Buttons row
+button_frame = ttk.Frame(root)
+button_frame.pack(pady=5)
+
+ttk.Button(button_frame, text="🧹 Clear", command=clear_fields).grid(row=0, column=0, padx=5)
+ttk.Button(button_frame, text="📋 Copy Result", command=copy_result).grid(row=0, column=1, padx=5)
+ttk.Button(button_frame, text="💾 Save to File", command=save_to_file).grid(row=0, column=2, padx=5)
 
 root.mainloop()
